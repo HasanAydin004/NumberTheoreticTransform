64acfac75ddd6539c2ba89f0beafc6e4abce55d5
Hasan Aydin <Hasan.Aydin_1@student.hs-rm.de>
2025-07-12 17:22:14 +0200

    Testen ob der algorithmus funktioniert wie gewollt

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,14 @@
+use number_theoretic_transform::{forward_ntt_recursive, inverse_ntt_recursive};
+
 fn main() {
+    let input: Vec<i128> = (0..128).collect();
+    let root = 17;
+    let modulus = 3329;
+    let ntt = forward_ntt_recursive(&input, root, modulus);
+    println!("{:?}", ntt);
+    let invntt = inverse_ntt_recursive(&ntt, root, modulus);
+    if input == invntt {
+        println!("Alles passt");
+        println!("{:?}", inverse_ntt_recursive(&ntt,root, modulus));
+    }
 }
